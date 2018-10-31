cf00080abea0726122e1b058fe3f98f8c3a17b4a
Eric Coulter <jecoulte@iu.edu>
2018-10-31 20:21:16 +0000

    script for running the ansible playbooks as slurm user

--- slurm_resume_ansible.sh
+++ slurm_resume_ansible.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-log_loc=/var/log/slurm/slurm_elastic.log
+log_loc=/var/log/slurm/slurm_resume.log
 
 #Get hostlist from slurm, add ", ", remove last ",", and wrap in ['s for ansible list format
 ansible_hosts="[ \"$(scontrol show hostnames $1 | tr '\n' ',' | sed 's/,$//' | sed 's/,/\", \"/g')\" ]"
 
 echo "$(date) Resuming $ansible_hosts :" >> $log_loc
 
-ansible-playbook -e "{ compute_instance_list: ${ansible_hosts} }" /etc/slurm/create_nodes.yml 2>&1 >> $log_loc
+count=1
+declare -i count
+
+ansible_result=1
+
+until [[ "${ansible_result}" = 0 ]] || [[ $count -ge 4 ]]; 
+do
+  echo "$(date) Playbook run $count:" >> $log_loc
+  ansible-playbook -e "{ compute_instance_list: ${ansible_hosts} }" /etc/slurm/computes.yml 2>&1 >> $log_loc
+  ansible_result=$?
+  echo "$(date) ansible_result: $ansible_result" >> $log_loc
+  count+=1
+done
+
+echo "$(date) Resume finished after $((count-1)) tries" >> $log_loc
 
